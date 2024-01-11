8abddccaa5df72a1cf9cc9a924430fafe97b8a93
Vitor Ribeiro <129464469+vitorribeirog@users.noreply.github.com>
2024-01-11 16:01:07 -0300

    Update script.php

--- php/script.php
+++ php/script.php
@@ -4,11 +4,11 @@ $nameUser = addslashes($_POST['name']);
 $emailUser = addslashes($_POST['email']);
 $phoneUser = addslashes($_POST['phone']);
 
-$to = "seuemail@gmail.com"; // email que recebe as solicitações de contato
+$to = "marcelaluizaoliveira1234@gmail.com"; // email que recebe as solicitações de contato
 $subject = "Coleta de dados - Meu Site";
 
 $body = "Nome: " . $nameUser . "\n" . "E-mail: " . $emailUser . "\n" . "Telefone: " . $phoneUser;
-$head = "From: emailfrom@gmail.com" . "\n" . "Reply-to: " . $emailUser . "\n" . "X=Mailer:PHP/" . phpversion(); // email que envia as solicitações
+$head = "From: betinasophiaelzabarros@gmail.com" . "\n" . "Reply-to: " . $emailUser . "\n" . "X=Mailer:PHP/" . phpversion(); // email que envia as solicitações
 
 if (mail($to, $subject, $body, $head)) {
     echo("E-mail enviado com sucesso!");
